01703ccad7fed635e28027c2b719bf5bd255a29d
Koshiro Yoshimoto <yoshimoto.koshiro@gmail.com>
2021-12-22 23:52:34 +0900

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,5 +34,13 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-plugin-mdx`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `src/posts`,
+        name: `posts`,
+      },
+    },
   ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,37 @@
-exports.createPages = async ({ actions }) => {
+const { create } = require("domain")
+const path = require("path")
+
+exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+
+  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
+
+  const result = await graphql(`
+  {
+    allMdx {
+      edges {
+        node {
+          frontmatter {
+            title
+            slug
+          }
+        }
+      }
+    }
+  }
+  `)
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GeaphQL query.`)
+    return
+  }
+
+  result.data.allMdx.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.slug,
+      component: blogPostTemplate,
+      context: {
+        slug: node.frontmatter.slug,
+      }
+    })
   })
 }
